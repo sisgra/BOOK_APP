1e76b8258056ba5e5f0c041a62ed981723391745
sis gra <grachova.silvia@gmail.com>
2021-06-22 14:03:09 +0200

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,69 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,SafeAreaView } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state={
+      totalCount:0,
+      readingCount:0,
+      readCount:0,
+    };
+  }
+  render(){
+    return(
+      <View style={{flex:1}}>
+        <SafeAreaView/>
+        <View
+          style={{
+            height:70,
+            borderBottomWidth:0.5,
+            alignItems:'center',
+            justifyContent:'center'
+          }}
+          >
+            <Text style={{fontSize:24}}>Book</Text>
+          </View>
+          <View style={{flex:1}}/>
+          <View 
+            style={{
+              height:70,
+              borderTopWidth:0.5,
+              borderTopColor:'#E9E9E9',
+              flexDirection:'row'
+            }}>
+              <View
+                style={{
+                  flex:1,
+                  alignItems:'center',
+                  justifyContent:'center'
+                }}>
+                  <Text style={{fontSize:20}}>TOTAL</Text>
+                  <Text>{this.state.totalCount}</Text>
+                </View>
+                <View 
+                  style={{
+                    flex:1,
+                    alignItems:'center',
+                    justifyContent:'center'
+                  }}>
+                     <Text style={{fontSize:20}}>READING</Text>
+                  <Text>{this.state.readingCount}</Text>
+                  </View>
+
+                  <View
+                  style={{
+                  flex:1,
+                  alignItems:'center',
+                  justifyContent:'center'
+                }}>
+                  <Text style={{fontSize:20}}>READ</Text>
+                  <Text>{this.state.readCount}</Text>
+                </View>
+            </View>
+      </View>
+    )
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
